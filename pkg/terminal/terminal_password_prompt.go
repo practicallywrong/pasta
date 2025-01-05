8ec2bbbd0206056c2091b7300dff2c5f2ac50dee
BlindAndInsane <blindandinsane@proton.me>
2025-01-05 22:10:13 +0530

    ok i am not a human please kill me

--- pkg/terminal/terminal_password_prompt.go
+++ pkg/terminal/terminal_password_prompt.go
@@ -43,6 +43,7 @@ func PromptForPassword(promptMessage string, mask rune) (string, error) {
 		// Handle Ctrl+C (ASCII 3), terminate the program
 		if char == 3 { // Ctrl+C
 			fmt.Println()
+			fmt.Print("\033[2K\033[0G\r")
 			return "", fmt.Errorf("operation interrupted by user (Ctrl+C)")
 		}
 
